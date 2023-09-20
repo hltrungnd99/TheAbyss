77a4a08c1aec5fcc443ca680d7bf6a9a95502792
hnducnd98 <37649895+hnducnd98@users.noreply.github.com>
2023-09-20 22:11:47 +0700

    nhan vat test, model

---
+++ Assets/_Games/_Scripts/Controllers/CharacterController.cs
@@ -0,0 +1,116 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField] protected Rigidbody myRig;
+
+    [SerializeField] private StatsCharacterBase statsCharacterBase;
+    [SerializeField] private Animator animatorCharacter;
+
+    private Vector3 directionMove = Vector3.zero;
+    private bool isCanMove;
+    private string currentStateAnim;
+
+    public StatCharacter statCharacter;
+
+    private void Start()
+    {
+        statCharacter = statsCharacterBase.statCharacter;
+    }
+
+    private void Update()
+    {
+#if UNITY_EDITOR
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            directionMove.z = 1;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            directionMove.z = -1;
+        }
+
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S))
+        {
+            directionMove.z = 0;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            directionMove.x = -1;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            directionMove.x = 1;
+        }
+
+        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
+        {
+            directionMove.x = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Attack();
+        }
+
+        Rotate();
+        Move();
+
+#endif
+    }
+
+    private void Move()
+    {
+        if (directionMove.magnitude < 0.05f)
+        {
+            if (currentStateAnim != "idle")
+            {
+                currentStateAnim = "idle";
+                animatorCharacter.SetTrigger("idle");
+            }
+            myRig.velocity = Vector3.zero;
+        }
+        else
+        {
+            myRig.velocity = directionMove * statCharacter.statCharacterOther.MSPD;
+
+            if (directionMove.magnitude < 0.6f)
+            {
+                if (currentStateAnim != "walk")
+                {
+                    currentStateAnim = "walk";
+                    animatorCharacter.SetTrigger("walk");
+                }
+            }
+            else
+            {
+                if (currentStateAnim != "run")
+                {
+                    currentStateAnim = "run";
+                    animatorCharacter.SetTrigger("run");
+                }
+            }
+        }
+
+    }
+
+    private void Rotate()
+    {
+        if (directionMove.magnitude >= 0.05f)
+        {
+            var angle = Mathf.Atan2(directionMove.x, directionMove.z) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, angle, 0), Time.deltaTime * 10);
+        }
+    }
+
+    private void Attack()
+    {
+        animatorCharacter.SetTrigger("attack");
+    }
+}
---
+++ Assets/_Games/_Scripts/GameTools/EventDispatcher.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class EventDispatcher
+{
+    public static Dictionary<EventName, List<Action<Component, object[]>>> _eventDictionary = new Dictionary<EventName, List<Action<Component, object[]>>>();
+
+    public static void RegisterEventListener(this MonoBehaviour monoBehaviour, EventName eventName, Action<Component, object[]> listener)
+    {
+        if (_eventDictionary.TryGetValue(eventName, out List<Action<Component, object[]>> lstEvent))
+        {
+            lstEvent.Add(listener);
+            _eventDictionary[eventName] = lstEvent;
+        }
+        else
+        {
+            lstEvent.Add(listener);
+            _eventDictionary.Add(eventName, lstEvent);
+        }
+    }
+
+    public static void RemoveEventListener(this MonoBehaviour monoBehaviour, EventName eventName, Action<Component, object[]> listener)
+    {
+        if (_eventDictionary.TryGetValue(eventName, out List<Action<Component, object[]>> lstEvent))
+        {
+            if (lstEvent.Contains(listener))
+            {
+                lstEvent.Remove(listener);
+            }
+            _eventDictionary[eventName] = lstEvent;
+        }
+    }
+
+    public static void PostEvent(this MonoBehaviour monoBehaviour, EventName eventName, object[] objs = null)
+    {
+        if (_eventDictionary.TryGetValue(eventName, out List<Action<Component, object[]>> lstEvent))
+        {
+            for (int i = 0; i < lstEvent.Count; i++)
+            {
+                lstEvent[i].Invoke(monoBehaviour, objs);
+            }
+        }
+    }
+}
+
+public enum EventName
+{
+    ATTACK
+}

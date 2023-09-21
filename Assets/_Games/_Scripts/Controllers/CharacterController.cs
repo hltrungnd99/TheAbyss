f48d6392fb9e9dcf4c97696994e50aa3f1b751d6
hnducnd98 <37649895+hnducnd98@users.noreply.github.com>
2023-09-21 22:26:26 +0700

    sua ty base character

--- Assets/_Games/_Scripts/Controllers/CharacterController.cs
+++ Assets/_Games/_Scripts/Controllers/CharacterController.cs
@@ -1,68 +1,35 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CharacterController : MonoBehaviour
 {
     [SerializeField] protected Rigidbody myRig;
+    [SerializeField] protected StatsCharacterBase statsCharacterBase;
+    [SerializeField] protected Animator animatorCharacter;
 
-    [SerializeField] private StatsCharacterBase statsCharacterBase;
-    [SerializeField] private Animator animatorCharacter;
+    protected Vector3 directionMove = Vector3.zero;
+    protected bool isCanMove;
 
-    private Vector3 directionMove = Vector3.zero;
-    private bool isCanMove;
     private string currentStateAnim;
 
     public StatCharacter statCharacter;
 
+    private StateMachine currentStateMachine;
+
     private void Start()
     {
-        statCharacter = statsCharacterBase.statCharacter;
+        statCharacter.Clone(statsCharacterBase.statCharacter);
 
         SetupStart();
     }
 
     private void Update()
     {
-#if UNITY_EDITOR
-
-        if (Input.GetKey(KeyCode.W))
+        if (currentStateMachine != null)
         {
-            directionMove.z = 1;
+            currentStateMachine.OnExcute();
         }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            directionMove.z = -1;
-        }
-
-        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.S))
-        {
-            directionMove.z = 0;
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            directionMove.x = -1;
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            directionMove.x = 1;
-        }
-
-        if (Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.D))
-        {
-            directionMove.x = 0;
-        }
-
-        if (Input.GetKeyDown(KeyCode.K))
-        {
-            Attack();
-        }
-
-        Rotate();
-        Move();
-
-#endif
+        SetupUpdate();
     }
 
     protected virtual void SetupStart()
@@ -75,57 +42,62 @@ public class CharacterController : MonoBehaviour
 
     }
 
-    private void Move()
+    //private void Move()
+    //{
+    //    if (directionMove.magnitude < 0.05f)
+    //    {
+    //        if (currentStateAnim != "idle")
+    //        {
+    //            currentStateAnim = "idle";
+    //            animatorCharacter.SetTrigger("idle");
+    //        }
+    //        myRig.velocity = Vector3.zero;
+    //    }
+    //    else
+    //    {
+    //        myRig.velocity = directionMove * statCharacter.statCharacterOther.MSPD;
+
+    //        if (directionMove.magnitude < 0.6f)
+    //        {
+    //            if (currentStateAnim != "walk")
+    //            {
+    //                currentStateAnim = "walk";
+    //                animatorCharacter.SetTrigger("walk");
+    //            }
+    //        }
+    //        else
+    //        {
+    //            if (currentStateAnim != "run")
+    //            {
+    //                currentStateAnim = "run";
+    //                animatorCharacter.SetTrigger("run");
+    //            }
+    //        }
+    //    }
+
+    //}
+
+    //private void Rotate()
+    //{
+    //    if (directionMove.magnitude >= 0.05f)
+    //    {
+    //        var angle = Mathf.Atan2(directionMove.x, directionMove.z) * Mathf.Rad2Deg;
+    //        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, angle, 0), Time.deltaTime * 10);
+    //    }
+    //}
+
+    //private void Attack()
+    //{
+    //    animatorCharacter.SetTrigger("attack");
+    //}
+
+    protected void ChangeState(StateMachine stateMachine)
     {
-        if (directionMove.magnitude < 0.05f)
+        if (currentStateMachine != stateMachine)
         {
-            if (currentStateAnim != "idle")
-            {
-                currentStateAnim = "idle";
-                animatorCharacter.SetTrigger("idle");
-            }
-            myRig.velocity = Vector3.zero;
+            if (currentStateMachine != null) currentStateMachine.OnExit();
+            currentStateMachine = stateMachine;
+            if (currentStateMachine != null) currentStateMachine.OnStart();
         }
-        else
-        {
-            myRig.velocity = directionMove * statCharacter.statCharacterOther.MSPD;
-
-            if (directionMove.magnitude < 0.6f)
-            {
-                if (currentStateAnim != "walk")
-                {
-                    currentStateAnim = "walk";
-                    animatorCharacter.SetTrigger("walk");
-                }
-            }
-            else
-            {
-                if (currentStateAnim != "run")
-                {
-                    currentStateAnim = "run";
-                    animatorCharacter.SetTrigger("run");
-                }
-            }
-        }
-
-    }
-
-    private void Rotate()
-    {
-        if (directionMove.magnitude >= 0.05f)
-        {
-            var angle = Mathf.Atan2(directionMove.x, directionMove.z) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, angle, 0), Time.deltaTime * 10);
-        }
-    }
-
-    private void Attack()
-    {
-        animatorCharacter.SetTrigger("attack");
-    }
-
-    protected void ChangeState()
-    {
-
     }
 }

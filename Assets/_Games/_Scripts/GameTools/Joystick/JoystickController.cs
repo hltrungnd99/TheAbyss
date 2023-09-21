b1c2bf22e00c4fd56e854b04fcdc96a3f9660a28
quynhtv2505 <quynhtv@gearinc.com>
2023-09-21 22:06:48 +0700

    Add_Joystick_ControllerPlayer
    add joystick and edit some folder

---
+++ Assets/_Games/_Scripts/GameTools/Joystick/JoystickController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickController : MonoBehaviour {
+
+  [SerializeField] protected RectTransform joystickBG;
+  [SerializeField] protected RectTransform joystickController;
+  [SerializeField] protected GameObject joystickPanel;
+  [SerializeField] protected float magnitude;
+
+  protected Vector3 direction;
+  public Vector3 Direction { get => direction; set => direction = value; }
+
+  protected Vector3 screen;
+  protected Vector3 mousePos => Input.mousePosition - screen / 2;
+  protected Vector3 startPos;
+  protected Vector3 updatePos;
+
+  protected void Start() {
+
+    screen.x = Screen.width;
+    screen.y = Screen.height;
+    joystickPanel.gameObject.SetActive(false);
+  }
+
+
+  private void Update() {
+    if (Input.GetMouseButtonDown(0)) {
+      joystickPanel.gameObject.SetActive(true);
+      startPos = mousePos;
+      joystickBG.anchoredPosition = startPos;
+    }
+    if (Input.GetMouseButton(0)) {
+      updatePos = mousePos;
+      joystickController.anchoredPosition = Vector3.ClampMagnitude((updatePos - startPos), magnitude) + startPos;
+    }
+    if (Input.GetMouseButtonUp(0)) {
+      direction = Vector3.zero;
+      joystickPanel.gameObject.SetActive(false);
+    }
+  }
+  private void OnDisable() {
+    direction = Vector3.zero;
+  }
+}
